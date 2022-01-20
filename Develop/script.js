62a84de926d3539e27eca4d8e29bd25c91c4928f
Alex C <cxz980314@gmail.com>
2022-01-20 16:18:08 -0500

    update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -9,33 +9,46 @@ function writePassword() {
   passwordText.value = password;
 }
 
-
+// This function will ask user to enter the password length (minimum 8 up to maximum 128),
+// If the length in this range, then it will call the "passwordCriteria()" otherwise it will show the 
+// alert message. At last, it will shuffle the generated password and show it in the text area
 function generatePassword() {
   let passwordLength = prompt("Please Enter Your Password Length (8~128)");
   if(8 <= passwordLength && 128 >= passwordLength) {
-    let test = passwordCriteria();
-    let long_Password = test[0];
-    let short_Password = test[1];
-    let r = shuffleString(passwordLength,long_Password,short_Password);
-    // alert(r);
+    //password_Object stores the returned object of passwordCriteria()
+    let password_Object = passwordCriteria();
+    //long_Password stores all the possible characters strings that match the criteria
+    let long_Password = password_Object[0]; 
+    //short_Password picks one random character from each string that matches the criteria and store them
+    let short_Password = password_Object[1];
+    let result_Password = shuffleString(passwordLength,long_Password,short_Password);
+    return result_Password;
   } else {
     alert("Please make sure the number you entered in the range of 8~128");
   }
 }
 
+// This function lists all the possible characters, it will ask users to confirm 
+// whether or not to include lowercase, uppercase, numeric, and/or special characters in the password.
+// For each case, it will randomly pick one character from the corresponding string, to make sure the generated
+// password is contained at least one character from that type string.
 function passwordCriteria() {
   let uppercase = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
   let lowercase ="abcdefghijklmnopqrstuvwxyz";
   let number = "0123456789";
   let specialChar = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
+  // password stores all the possible characters
   let password = "";
-  let password_1 = ""
+  // password_1 stores one character from each chosen string
+  let password_1 = "";
+  // count is to make sure user chooses at least one criteria
   let count = 0; 
 
   let uppercase_Confirm = confirm("Do you want UpperCase Letters in your password?");
   if(uppercase_Confirm == true) {
     password += uppercase;
-    password_1 += uppercase[Math.floor(Math.random()*uppercase.length)];
+    //  Using Math.random() to randomly pick one character
+    password_1 += uppercase[Math.floor(Math.random()*uppercase.length)];  
     count++;
   }
 
@@ -60,6 +73,7 @@ function passwordCriteria() {
     count++
   }
   
+  // If user doesn't choose any criteria, the alert message will pop out.
   if(count > 0) {
     return [password, password_1];
   } else {
@@ -68,6 +82,8 @@ function passwordCriteria() {
   }
 }
 
+// According to the length and type of character, this function will generate the random password that meets the criteria
+// To make sure the password contain every selected type characters, it shuffles long_password and short_password
 function shuffleString(length, long, short) {
   let result = "";
   
@@ -79,7 +95,6 @@ function shuffleString(length, long, short) {
   let shuffled = result.split('').sort(function(){return 0.5-Math.random()}).join('');
   return shuffled;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
